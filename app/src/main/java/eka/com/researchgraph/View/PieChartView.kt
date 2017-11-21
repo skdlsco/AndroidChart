65dbc68182e7e5576889bc3cb15a0c53115df7e3
Eka <nicnic1234@naver.com>
2017-11-21 14:50:37 +0900

    modified PieChart animateType
    add animateType2

--- app/src/main/java/eka/com/researchgraph/View/PieChartView.kt
+++ app/src/main/java/eka/com/researchgraph/View/PieChartView.kt
@@ -9,6 +9,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.ViewPropertyAnimator
 import eka.com.researchgraph.R
@@ -22,7 +23,7 @@ class PieChartView : View {
     var innerStrokeWidth = 0f
     var outerRadius = 0f
     var innerRadius = 0f
-
+    var animateType = AnimateType.AnimateType1
     private var animateValue = 0f
 
     constructor(context: Context?) : super(context)
@@ -36,7 +37,7 @@ class PieChartView : View {
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
-        var startAngle = 180f
+        var startAngle = 270f
         val outerPaint = Paint()
         outerPaint.isAntiAlias = true
         outerPaint.style = Paint.Style.FILL
@@ -45,16 +46,39 @@ class PieChartView : View {
         outerCircle.set(width / 2 - outerRadius, height / 2 - outerRadius,
                 width / 2 + outerRadius, height / 2 + outerRadius)
 
+        when (animateType) {
+            AnimateType.AnimateType1 -> {
+                canvas?.let {
+                    for (element in elements) {
+                        outerPaint.color = element.color
+                        val sweepAngle = 360f / (100f / element.percentage)
+                        canvas.drawArc(outerCircle, startAngle, (sweepAngle + 0.1f) * animateValue, true, outerPaint)
+                        startAngle += sweepAngle
+                    }
+                }
+            }
+
+            AnimateType.AnimateType2 -> {
+                canvas?.let {
+                    for (element in elements) {
+                        outerPaint.color = element.color
+                        val sweepAngle = 360f / (100f / element.percentage)
+                        if (animateValue >= startAngle - 270f + sweepAngle) {
+                            canvas.drawArc(outerCircle, startAngle, (sweepAngle + 0.1f), true, outerPaint)
+                        } else {
+                            if ((sweepAngle) * (animateValue - (startAngle - 270f)) / sweepAngle > 0f)
+                                canvas.drawArc(outerCircle, startAngle, (sweepAngle + 0.1f) * (animateValue - (startAngle - 270f)) / sweepAngle, true, outerPaint)
+                        }
+                        startAngle += sweepAngle
+                    }
+                }
+            }
+        }
+
+        //draw innerCircle
         canvas?.let {
             val width = canvas.width
             val height = canvas.height
-
-            for (element in elements) {
-                outerPaint.color = element.color
-                val sweepAngle = 360f / (100f / element.percentage)
-                canvas.drawArc(outerCircle, startAngle, (sweepAngle + 0.1f) * animateValue, true, outerPaint)
-                startAngle += sweepAngle
-            }
 
             val innerPaint = Paint()
             innerPaint.style = Paint.Style.FILL
@@ -95,14 +119,32 @@ class PieChartView : View {
     }
 
     override fun animate(): ViewPropertyAnimator {
-        val animator = ValueAnimator.ofFloat(0f, 1f)
-        animator.duration = 1000
-        animator.addUpdateListener {
-            animateValue = it.animatedValue as Float
-            invalidate()
+        when (animateType) {
+            AnimateType.AnimateType1 -> {
+                val animator = ValueAnimator.ofFloat(0f, 1f)
+                animator.duration = 1000
+                animator.addUpdateListener {
+                    animateValue = it.animatedValue as Float
+                    invalidate()
+                }
+                animator.start()
+            }
+
+            AnimateType.AnimateType2 -> {
+                val animator = ValueAnimator.ofFloat(0f, 360f)
+                animator.duration = 1000
+                animator.addUpdateListener {
+                    animateValue = it.animatedValue as Float
+                    invalidate()
+                }
+                animator.start()
+            }
         }
-        animator.start()
         return super.animate()
+    }
+
+    enum class AnimateType {
+        AnimateType1, AnimateType2
     }
 
     class Element(var color: Int, var percentage: Float)
